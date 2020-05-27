7e3c7621457a0bb7138025e7896114b63979c4ff
ItsAMeNick <zonern@rpi.edu>
2020-05-27 17:36:07 -0400

    Updates

---
+++ precache-manifest.7a582a98aa57649c343a98465194af3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb5447edeb40f4155650f872df4081f",
+    "url": "/code-names/index.html"
+  },
+  {
+    "revision": "47137e440bd2bbdd780c",
+    "url": "/code-names/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "47137e440bd2bbdd780c",
+    "url": "/code-names/static/js/2.d219d11a.chunk.js"
+  },
+  {
+    "revision": "d1787ad1cd715bd2b4facd9c2077f98c",
+    "url": "/code-names/static/js/2.d219d11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42624f1d7f14caff578",
+    "url": "/code-names/static/js/main.4182da67.chunk.js"
+  },
+  {
+    "revision": "7fcfb0af21f0b1dcba71",
+    "url": "/code-names/static/js/runtime-main.9db82e6f.js"
+  }
+]);
