bc3a91977eb1b7e827da78472e243637d3480ab4
ItsAMeNick <zonern@rpi.edu>
2020-05-28 17:32:23 -0400

    Updates

---
+++ precache-manifest.a6e1b1e84006207ce7025abec5fc061e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22778f39a4031a35cbd329b16dfe4b22",
+    "url": "/code-names/index.html"
+  },
+  {
+    "revision": "da2b550a0f2001e7a52f",
+    "url": "/code-names/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "da2b550a0f2001e7a52f",
+    "url": "/code-names/static/js/2.e120dd48.chunk.js"
+  },
+  {
+    "revision": "d1787ad1cd715bd2b4facd9c2077f98c",
+    "url": "/code-names/static/js/2.e120dd48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46afb3b80814144d0f5",
+    "url": "/code-names/static/js/main.14c8cf90.chunk.js"
+  },
+  {
+    "revision": "7fcfb0af21f0b1dcba71",
+    "url": "/code-names/static/js/runtime-main.9db82e6f.js"
+  },
+  {
+    "revision": "0ed2998723a611016e4c7e610803275b",
+    "url": "/code-names/static/media/lose_blue.0ed29987.png"
+  },
+  {
+    "revision": "c9c9c4d25b7393da823ce3a24431d3ab",
+    "url": "/code-names/static/media/lose_red.c9c9c4d2.png"
+  },
+  {
+    "revision": "7fbba14c91f6ba8ac44752ac7d0b2b1f",
+    "url": "/code-names/static/media/win_blue.7fbba14c.png"
+  },
+  {
+    "revision": "b27b604180421d98e178976b27abe06b",
+    "url": "/code-names/static/media/win_red.b27b6041.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/code-names/precache-manifest.46129aea8c84e1c7243a0c03bd2b3a50.js"
+  "/code-names/precache-manifest.a6e1b1e84006207ce7025abec5fc061e.js"
 );
 
 self.addEventListener('message', (event) => {
